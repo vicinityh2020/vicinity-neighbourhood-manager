1da3ccdb527a87f2ec3d20afa723e771a0ac493c
Viktor <viktor.oravec@gmail.com>
2016-04-29 08:34:00 +0200

    Authentication updated

--- vicinityManager/server/routes/api.js
+++ vicinityManager/server/routes/api.js
@@ -11,17 +11,25 @@ router.post('/authenticate', function(req, res, next) {
   var response = {};
   var userName = req.body.username;
   var password = req.body.password;
-  
+
   if (userName && password) {
-    userAccountsOp.find({ email: userName }, function(error, result) {
+    userAccountsOp.find({ 'accountOf.email': userName }, function(error, result) {
       debugger;
       if (error || !result || result.length !== 1){
         response = { success: false };
       } else {
-        if ((userName === result[0].email) && (password === result[0].authentication.password)) {
-          var token = jwt.jwtEncode(userName, result[0].authentication.principalRoles, result[0].id);
-          response = { 
-            success: true, 
+        var accounts = result[0].accountOf;
+        // remove unnecessary accounts from results
+        for (var index = accounts.length - 1; index >= 0; index --) {
+            if (accounts[index].email !== userName){
+              accounts.splice(index,1);
+            }
+        }
+
+        if ((userName === result[0].accountOf[0].email) && (password === result[0].accountOf[0].authentication.password)) {
+          var token = jwt.jwtEncode(userName, result[0].accountOf[0].authentication.principalRoles, result[0].id);
+          response = {
+            success: true,
             message: token}
         } else {
           response = { success: false};
