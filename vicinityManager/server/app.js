6bf5c1e634386b7b89e4fa6b519ef7dabb09dde1
Viktor <viktor.oravec@gmail.com>
2016-06-11 14:16:22 +0200

    Configuration fixes

--- vicinityManager/server/app.js
+++ vicinityManager/server/app.js
@@ -31,7 +31,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-mongoose.connect('mongodb://vicinityManager:abc123@localhost/vicinity_neighbourhood_manager?authSource=admin', function(error){
+mongoose.connect(process.env.VCNT_MNGR_DB, function(error){
   if (error){
     console.log("VMModel: Couldn't connect to data source!" + error);
   } else {
