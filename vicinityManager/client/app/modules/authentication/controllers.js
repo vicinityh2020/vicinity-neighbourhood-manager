5dae5090c02db25a7c71dd9c14511578f87d59bf
Hana Hozzankova <hana.hozzankova@bavenir.eu>
2016-06-17 13:48:58 +0200

    show password and moving inputs work

--- vicinityManager/client/app/modules/authentication/controllers.js
+++ vicinityManager/client/app/modules/authentication/controllers.js
@@ -11,6 +11,36 @@ angular.module('Authentication')
                $scope.isError = false;
                $scope.visib = 'visible';
                $scope.visib2 = 'hidden';
+               $scope.showPass = "password";
+
+               $("#myCheck").prop("checked", false);
+               $("#pass").prop("type", "password");
+
+
+              (function ($) {
+                  $.toggleShowPassword = function (options) {
+                      var settings = $.extend({
+                          field: "#password",
+                          control: "#toggle_show_password",
+                      }, options);
+
+                      var control = $(settings.control);
+                      var field = $(settings.field)
+
+                      control.bind('click', function () {
+                          if (control.is(':checked')) {
+                              field.prop('type', 'text');
+                          } else {
+                              field.prop('type', 'password');
+                          }
+                      })
+                  };
+              }(jQuery));
+
+              $.toggleShowPassword({
+                field: '#pass',
+                control: '#myCheck'
+              });
 
                $scope.login = function() {
 
@@ -18,6 +48,10 @@ angular.module('Authentication')
 
                  AuthenticationService.Login($scope.username, $scope.password, function(response){
 
+                   var $user = $("#user");
+	                 var $pass = $("#pass");
+	                 var $button = $("#login-button");
+                  //  var $warButton = $("#warButt");
 
                    if(response.success){
                     //  Notification.success("Welcome to Vicinity!");
@@ -27,9 +61,18 @@ angular.module('Authentication')
                    } else {
                      $scope.error = "Incorrect email or password";
 
+                     $user.addClass("invalid");
+			               $pass.addClass("invalid");
+
                     //  Notification.error("Incorrect email or password");
                      $scope.isError = true;
                      $scope.dataLoading = false;
+
+                     setTimeout(function() {
+                      $user.removeClass("invalid");
+			                $pass.removeClass("invalid");
+                      $scope.dataLoading = false;
+                    }, 2000);
 
                     //  setTimeout(function() {
                     //   $('#zmiz').hide('fast');
