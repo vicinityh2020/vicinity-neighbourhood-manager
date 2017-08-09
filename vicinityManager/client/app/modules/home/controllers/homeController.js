ce70f324f5934c9969d9044c10de1b22c1a18625
Jorge Almela <jorge.almela@bavenir.eu>
2017-08-09 16:02:35 +0200

    Shows goToTop only if document is higher than window

--- vicinityManager/client/app/modules/home/controllers/homeController.js
+++ vicinityManager/client/app/modules/home/controllers/homeController.js
@@ -1,13 +1,16 @@
 'use strict';
 angular.module('VicinityManagerApp.controllers')
 .controller('homeController',
-            ['$scope', '$window', 'Base64','tokenDecoder',
+            ['$scope', '$window', 'Base64','tokenDecoder', '$interval',
+            function ($scope, $window, Base64, tokenDecoder, $interval) {
 
-            function ($scope, $window, Base64, tokenDecoder) {
               $(window).trigger('resize');
+
+              $interval(checkScroll, 1000); // Checks if it is necessary to display goToTop
 
               $scope.isDev = false;
               $scope.isInfOp = false;
+              $scope.isScrollable = false;
 
               var myInit = function(){
                 var payload = tokenDecoder.deToken();
@@ -26,9 +29,17 @@ angular.module('VicinityManagerApp.controllers')
               myInit();
 
     // Scroll to top
-    
     $scope.goToTop = function(){
         $window.scrollTo(0, 0);
     };
+
+    function checkScroll(){
+      if( $(window).height() < $(document).height() ) {
+        $scope.isScrollable = true;
+      } else {
+        $scope.isScrollable = false;
+      }
+    }
+
 
 }]);
