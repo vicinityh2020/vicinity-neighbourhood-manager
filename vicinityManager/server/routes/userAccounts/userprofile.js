e76fdb42f885f5523d2160d712f4ab48c1644e56
Jorge Almela <jorge.almela@bavenir.eu>
2017-11-06 09:33:19 +0100

    Merge branch 'master' of bitbucket.org:bavenir/vicinity
    Solving merge problem with userProfile, removing from view deleted orgs

--- vicinityManager/server/routes/userAccounts/userprofile.js
+++ vicinityManager/server/routes/userAccounts/userprofile.js
@@ -16,26 +16,16 @@ function getAllFilteredUserAccountsFacade(req, res, next) {
   var type = req.query.type;
 
   if(Number(type) === 0){
-<<<<<<< Updated upstream
     userAccountOp.find({status: {$exists: false}}) // if the field status exists, is also equal to deleted
-=======
-    userAccountOp.find({})
->>>>>>> Stashed changes
     .then( function(data) { res.json({"error": false, "message": data}); })
     .catch( function(err) { res.json({"error": true, "message": "Error fetching data"}); });
   } else {
     userAccountOp.findById(o_id, {knows: 1})
     .then( function(data){
       var qry;
-<<<<<<< Updated upstream
       if(Number(type) === 1){ qry = {_id: {$in: data.knows}, status: {$exists: false} }; }
       else { qry = {_id: {$not: {$in: data.knows} }, status: {$exists: false} }; }
       return userAccountOp.find(qry); // if the field status exists, is also equal to deleted
-=======
-      if(Number(type) === 1){ qry = {$in: data.knows}; }
-      else { qry = {$not: {$in: data.knows}}; }
-      return userAccountOp.find({_id: qry});
->>>>>>> Stashed changes
     })
     .then( function(data){res.json({"error": false, "message": data});})
     .catch( function(err){res.json({"error": true, "message": "Error fetching data"});});
