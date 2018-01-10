eca3ae47ce846165c7e5a2bc94ed329d4d266b6c
Jorge Almela <jorge.almela@bavenir.eu>
2018-01-10 14:16:34 +0100

    Remove old server logic for sharing items

--- vicinityManager/server/routes/items.js
+++ vicinityManager/server/routes/items.js
@@ -3,11 +3,6 @@ var router = express.Router();
 
 var putHelper = require('./items/put.js');
 var getHelper = require('./items/get.js');
-var itemAccess = require('./items/processItemAccess.js');
-var cancelRequest = require('./items/cancelItemRequest.js');
-var acceptRequest = require('./items/acceptItemRequest.js');
-var rejectRequest = require('./items/rejectItemRequest.js');
-var cancelAccess = require('./items/cancelItemAccess.js');
 var deleteHelper = require('./items/delete.js');
 
 router
@@ -15,11 +10,6 @@ router
 // When we receive item id to process some action over it
   .get('/:id', getHelper.getItemWithAdd)
   .put('/:id', putHelper.putOne)
-  .put('/:id/access', itemAccess.processItemAccess)
-  .put('/:id/access/cancelRequest', cancelRequest.cancelItemRequest)
-  .put('/:id/access/accept', acceptRequest.acceptItemRequest)
-  .put('/:id/access/reject', rejectRequest.rejectItemRequest)
-  .put('/:id/access/cancel', cancelAccess.cancelItemAccess)
   .post('/delete/:id', deleteHelper.deleteOne)
 
 // When userAccount requires devices/services
