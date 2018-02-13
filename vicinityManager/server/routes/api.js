a722ec72ec1189d6a0d16d8597c1b67dc085b5ba
Jorge Almela <jorge.almela@bavenir.eu>
2018-02-13 14:54:53 +0100

    Api routes defined

--- vicinityManager/server/routes/api.js
+++ vicinityManager/server/routes/api.js
@@ -1,14 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
-// var postHelper = require('./api/post.js');
-// var putHelper = require('./api/put.js');
+var apiController = require('../controllers/api/api.js');
 
-// router
-//   .post('/authenticate', postHelper.authenticate)
-//   .post('/recovery', postHelper.findMail)
-//   .post('/remember', postHelper.rememberCookie)
-//   .put('/recovery/:id', putHelper.updatePwd)
-//   .put('/remember/:id', putHelper.updateCookie);
+router
+//Authenticate
+  .post('/authenticate', apiController.authenticate)
+//Organisation
+  .get('/organisation/:id/friends', apiController.getFriends)
+  .get('/organisation/:id/users', apiController.getUsers)
+  .get('/organisation/:id/devices', apiController.getDevices)
+  .get('/organisation/:id/services', apiController.getServices)
+  .post('/organisation', apiController.createOrganisation)
+  .delete('/organisation', apiController.removeOrganisation)
+//Users
+  .get('/users/:id', apiController.getUser)
+  .get('/users/:id/items', apiController.getUserItems)
+  .post('/users/', apiController.createUser)
+  .put('/users/:id', apiController.updateUser) // enable: true or false; Let other updates?
+  .put('/users/:id/enabled', apiController.enableItem)
+  .delete('/users/:id', apiController.removeUser)
+//Items
+  .get('/items/:id', apiController.getItem)
+  .post('/items', apiController.createItem)
+  .put('/items', apiController.updateItem)
+  .delete('/items/:id', apiController.removeItem)
+//Agents
+  .get('/agents/:id/items', apiController.getAgentUsers)
+  .post('/agents/', apiController.createAgent)
+  .delete('/agents/:id', apiController.removeAgent)
+//Contracts
+  .get('/partnership', apiController.partnershipFeeds)
+  .post('/partnership', apiController.requestPartnership)
+  .put('/partnership', apiController.managePartnership) // In payload --> accept, reject, cancel
+//Friending
+  .get('/contract', apiController.contractFeeds)
+  .post('/contract', apiController.requestContract)
+  .put('/contract', apiController.manageContract); // In payload --> accept, reject, cancel
 
 module.exports = router;
