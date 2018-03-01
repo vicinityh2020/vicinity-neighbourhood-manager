f3185efa36b36868abb80215ab6b10b589ea0359
Jorge Almela <jorge.almela@bavenir.eu>
2018-03-01 16:20:29 +0100

    If user deleted, all items under it are disabled

--- vicinityManager/server/services/users/deleteUsers.js
+++ vicinityManager/server/services/users/deleteUsers.js
@@ -2,10 +2,13 @@
 
 var mongoose = require('mongoose');
 var userOp = require('../../models/vicinityManager').user;
+var itemOp = require('../../models/vicinityManager').item;
 var userAccountOp = require('../../models/vicinityManager').userAccount;
 var logger = require('../../middlewares/logger');
 var sync = require('../../services/asyncHandler/sync');
 var audits = require('../../controllers/audit/put');
+var commServer = require('../../services/commServer/request');
+var ctService = require('../../services/contracts/contracts');
 
 // Public functions
 
@@ -55,6 +58,7 @@ function isMyUser(cid, uid){
     });
   });
 }
+
 // Private functions
 
 /*
@@ -74,9 +78,18 @@ function deleting(id, otherParams, callback){
     hasContracts: [],
     cid: {}
   };
-  userOp.findOne({_id: id})
+  userOp.findOne({_id: id}, {cid:1, hasItems:1}).populate('hasItems.id', 'cid oid')
   .then(function(response){
-    cid = response.cid;
+    var aux = response.toObject();
+    var toDisable = [];
+    cid = aux.cid;
+    for(var i = 0; i < aux.hasItems.length; i++){
+      toDisable.push(aux.hasItems[i].id);
+    }
+    disableUserItems(toDisable, otherParams.userMail)
+    .then(function(response){ logger.debug('Disable items finished'); })
+    .catch(function(err){ logger.debug('Disable items error: ' + err); });
+
     return userOp.update({_id: id}, { $set: obj });
   })
   .then(function(response){
@@ -97,6 +110,61 @@ function deleting(id, otherParams, callback){
     logger.error({user: otherParams.userMail, action: 'deleteUser', item: id, message: JSON.stringify(error)});
     callback(id, "Error: " + error);
   });
+}
+
+/*
+Disables all user items when the user is removed
+*/
+function disableUserItems(items, mail){
+  return new Promise(function(resolve, reject) {
+    if(items.length > 0){ // Check if there is any item to delete
+      // logger.debug('Start async handler...');
+      sync.forEachAll(items,
+        function(value, allresult, next, otherParams) {
+          disabling(value, otherParams, function(value, result) {
+              // logger.debug('END execution with value =', value, 'and result =', result);
+              allresult.push({error: value, message: result});
+              next();
+          });
+        },
+        function(allresult) {
+          if(allresult.length === items.length){
+            // logger.debug('Completed async handler: ' + JSON.stringify(allresult));
+              resolve(allresult);
+          }
+        },
+        false,
+        {mail: mail}
+      );
+    } else {
+      logger.warn({user:mail, action: 'deleteUser', message: "No items to be removed"});
+      reject("Nothing to be removed...");
+    }
+  });
+}
+
+// Disables devices one by one
+function disabling(data, otherParams, callback){
+  commServer.callCommServer({}, 'users/' + data.oid + '/groups/' + data.cid.extid + '_ownDevices', 'DELETE')
+    .then(function(response){
+     var query = {status: 'disabled', accessLevel: 0};
+     return itemOp.update({ _id : data._id}, {$set: query});
+   })
+   .then(function(response){
+     return itemOp.findOne({ _id : data._id}).populate('cid.id', 'knows');
+   })
+   .then(function(response){
+     item = response.toObject();
+     ctService.removeDevice(item, {}, function(err, response){
+       if(!err){
+         logger.audit({user: otherParams.mail, action: 'DisableItem', item: data._id });
+         callback(false, response);
+       } else {
+         logger.error({user: otherParams.mail, action: 'DisableItem', item: data._id, message: err});
+         callback(true, err);
+       }
+     });
+   });
 }
 
 // Export modules
