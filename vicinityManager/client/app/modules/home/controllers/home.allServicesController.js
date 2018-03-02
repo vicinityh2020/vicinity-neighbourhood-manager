ada9ece9ff55c1d0a5ed538a6cbb2c4b810ade4e
Jorge Almela <jorge.almela@bavenir.eu>
2018-03-02 15:13:57 +0100

    UI update services view; can request same service more than once; service contracts counter

--- vicinityManager/client/app/modules/home/controllers/home.allServicesController.js
+++ vicinityManager/client/app/modules/home/controllers/home.allServicesController.js
@@ -40,7 +40,7 @@ angular.module('VicinityManagerApp.controllers')
           for(var i = 0; i < response.data.message.length; i++){
             for(var j = 0; j < response.data.message[i].hasContracts.length; j++){
               if(response.data.message[i].hasContracts[j].contractingUser.toString() === $scope.myUserId.toString()){
-                response.data.message[i].contracted = true;
+                response.data.message[i].contracted += 1;
               }
             }
               $scope.items.push(response.data.message[i]);
