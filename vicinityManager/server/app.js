e81cfdcaf1c8c1c209ec5b27430c9593432bc00c
Jorge Almela <jorge.almela@bavenir.eu>
2018-04-12 09:16:44 +0200

    Enable SSL communication to Node JS app

--- vicinityManager/server/app.js
+++ vicinityManager/server/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var helmet = require("helmet"); // Forcing SSL
 //var winston = require('winston');
 
 // ROUTES Import
@@ -44,7 +45,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(helmet());
 
 /*
 Endpoints
