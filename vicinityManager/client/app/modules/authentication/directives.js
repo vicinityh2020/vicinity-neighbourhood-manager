f8007eabcd4110f6d070d752b17cc85d4a63662f
Jorge Almela <jorgealmela@Jorges-MacBook-Pro.local>
2018-09-25 12:41:28 +0200

    Add terms of service

---
+++ vicinityManager/client/app/modules/authentication/directives.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('Authentication')
+.directive('tos', function() {
+  return {
+    controller: 'LoginController',
+    templateUrl: "./modules/authentication/views/templates/tos.html"
+  };
+})
+.directive('modal', function() {
+  return {
+    controller: 'LoginController',
+    templateUrl: "./modules/authentication/views/templates/modal.html"
+  };
+});
