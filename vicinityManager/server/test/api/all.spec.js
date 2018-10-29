76a56486fa821d4160635bbfe6ff2db5ce5e56ca
Jorge Almela <jorgealmela@Jorges-MacBook-Pro.local>
2018-10-29 18:08:51 +0100

    Add users mgmt to all cases test scenario

--- vicinityManager/server/test/api/all.spec.js
+++ vicinityManager/server/test/api/all.spec.js
@@ -19,16 +19,24 @@ chai.use(chaiHttp);
 // Global variables
 var token;
 var login;
+var uid;
 
 // tests
 // before(function() {
 // });
 
-describe('Organisation test scenarios', function(){
+// TODO test all error codes and special cases
+describe('Full test scenario', function(){
   it('Generate token...', loginSuccess);
   it('it should create a organisation', createOrganisation);
   it('Generate new token...', loginSuccess);
   it('it should get a organisation', getOrganisation);
+  it('it should get user info', getUser);
+  it('it should update user name', updateUserMetadata);
+  it('it should update user roles', updateUserRole);
+  it('it should update user visibility', updateUserVisibility);
+  // it('it should create a node', );
+  // it('it should create a node', );
   it('it should remove a organisation', removeOrganisation);
 });
 
@@ -105,3 +113,77 @@ function createOrganisation(done){
           done();
         });
       }
+
+  function getUser(done){
+    chai.request(server)
+      .get('/api/users')
+      .set('x-access-token', token)
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.message.should.have.property('_id');
+        res.body.message._id.should.be.a('string');
+        res.body.message.should.have.property('cid');
+        res.body.message.cid.should.be.a('object');
+        res.body.message.should.have.property('email');
+        res.body.message.email.should.be.a('string');
+        res.body.message.should.have.property('authentication');
+        res.body.message.authentication.should.be.a('object');
+        uid = res.body.message._id;
+        done();
+      });
+    }
+
+    function updateUserMetadata(done){
+      var data = {"data":{
+                    "name": "newName"
+                  },
+                  "type": "metadata"};
+      chai.request(server)
+        .put('/api/users/' + uid)
+        .set('x-access-token', token)
+        .send(data)
+        .end(function(err, res){
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.should.have.property('success');
+          res.body.success.should.equal(true);
+          done();
+        });
+      }
+
+      function updateUserVisibility(done){
+        var data = {"data":{
+                      "accessLevel": 2
+                    },
+                    "type": "visibility"};
+        chai.request(server)
+          .put('/api/users/' + uid)
+          .set('x-access-token', token)
+          .send(data)
+          .end(function(err, res){
+            res.should.have.status(201);
+            res.body.should.be.a('object');
+            res.body.should.have.property('success');
+            res.body.success.should.equal(true);
+            done();
+          });
+        }
+
+        function updateUserRole(done){
+          var data = {"data":{
+                        "roles": ['user', 'administrator', 'service provider', 'infrastructure operator', 'system integrator', 'device owner']
+                      },
+                      "type": "roles"};
+          chai.request(server)
+            .put('/api/users/' + uid)
+            .set('x-access-token', token)
+            .send(data)
+            .end(function(err, res){
+              res.should.have.status(201);
+              res.body.should.be.a('object');
+              res.body.should.have.property('success');
+              res.body.success.should.equal(true);
+              done();
+            });
+          }
