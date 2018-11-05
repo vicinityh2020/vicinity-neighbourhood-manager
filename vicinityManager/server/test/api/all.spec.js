ad0e1b5ae718fcabc2b2f11d39729e6448d2f497
Jorge Almela <jorgealmela@remoteaccess-wlc-guest2-ipv4-0020.misc.uni-kl.de>
2018-11-05 18:22:56 +0100

    Add organisation 2 to tests

--- vicinityManager/server/test/api/all.spec.js
+++ vicinityManager/server/test/api/all.spec.js
@@ -17,11 +17,11 @@ var uuid = require('uuid');
 chai.use(chaiHttp);
 
 // Global variables
-var token;
-var login;
-var uid;
-var adid;
-var oidDev;
+var token, token1, token2;
+var login1, login2;
+var uid1, uid2;
+var adid1, adid2;
+var oidDev, oidSer;
 
 // tests
 // before(function() {
@@ -33,42 +33,51 @@ describe('Full test scenario', function(){
   it('Get wrong username error', loginWrongName);
   it('Generate admin token...', loginSuccess);
   // it('it should get body validation error', createOrganisation);
-  it('it should create a organisation-1', createOrganisation);
-  it('Generate company-1 token...', loginSuccess);
-  it('it should get a organisation', getOrganisation);
-  it('it should get user info', getUser);
-  it('it should throw a nodeNoRoles error', createNodeNoRoles);
-  it('it should update user-1 name', updateUserMetadata);
-  it('it should update user-1 roles', updateUserRole);
-  it('Update company-1 token, new roles...', loginSuccess);
-  it('it should update user-1 visibility', updateUserVisibility);
-  it('it should throw a nodeNoPassword error', createNodeNoPassword);
-  it('it should create node-1', createNode);
-  it('it should get a deviceErrorWrongAgid when registering', registerDeviceNoAgid);
-  it('it should get a deviceErrorNoData when registering', registerDeviceNoData);
-  it('it should register a device', registerDevice);
-  it('it should enable a device', enableDevice);
-  // it('it should create a organisation-2', createOrganisation);
-  // it('Generate company-2 token...', loginSuccess);
+  it('Create a organisation-1', createOrganisation);
+  it('Generate organisation-1 token...', loginSuccess);
+  it('Get organisation-1 data', getOrganisation);
+  it('Get user-1 info', getUser);
+  it('Throw nodeNoRoles error', createNodeNoRoles);
+  it('Update user name', updateUserMetadata);
+  it('Update user-1 roles', updateUserRole1);
+  it('Update organisation-1 token, new roles...', loginSuccess);
+  it('Update user-1 visibility', updateUserVisibility1);
+  it('Throw a nodeNoPassword error', createNodeNoPassword);
+  it('Create node-1', createNode1);
+  it('Get a deviceErrorWrongAgid when registering', registerDeviceNoAgid);
+  it('Get a deviceErrorNoData when registering', registerDeviceNoData);
+  it('Register a device', registerDevice);
+  it('Enable a device', enableDevice);
+  it('it should create a organisation-2', createOrganisation);
+  it('Generate company-2 token...', loginSuccess);
+  it('Get user-2 info', getUser);
+  it('Update user-2 roles', updateUserRole2);
+  it('Update user-2 visibility', updateUserVisibility2);
+  it('Create node-2', createNode2);
+  it('Rregister a service', registerService);
+  it('Enable a device', enableService);
   // it('it should not find partnership req', createOrganisation);
   // it('it should request a partnership', createOrganisation);
   // it('it should check partnership requests', createOrganisation);
   // it('it should accept a partnership', createOrganisation);
-  // it('it should update user-2 roles', updateUserRole);
-  // it('it should update user-2 visibility', updateUserVisibility);
-  // it('it should create node-2', createNode);
-  // it('it should register a service', );
   // TODO check comm server calls
   // TODO Contracts tests -- change conditions to see reactions (i.e. remove cts)
-  it('it should remove a organisation-1', removeOrganisation);
-  // it('it should remove a organisation-2', removeOrganisation);
+  it('Remove organisation-1', removeOrganisation1);
+  it('Remove organisation-2', removeOrganisation2);
 });
 
 // Functions
 
 function loginSuccess(done){
   var data = {};
-  data.username = login || "admin@admin.com";
+  var login; // Decides which company should be logging in
+  if(!login2){
+    if(!login1){ login = "admin@admin.com"; }
+    else { login = login1; }
+  } else {
+    login = login2;
+  }
+  data.username = login;
   data.password = "password";
   chai.request(server)
       .post('/api/authenticate')
@@ -79,7 +88,13 @@ function loginSuccess(done){
         res.body.should.have.property('message');
         res.body.message.should.have.property('token');
         res.body.message.token.should.be.a('string');
-        token = res.body.message.token;
+        if(login === "admin@admin.com"){
+          token = res.body.message.token;
+        } else if(login === login1){
+          token1 = res.body.message.token;
+        } else {
+          token2 = res.body.message.token;
+        }
         done();
       });
 }
@@ -136,7 +151,11 @@ function createOrganisation(done){
       res.body.should.be.a('object');
       res.body.message.should.have.property('login');
       res.body.message.login.should.be.a('string');
-      login = res.body.message.login;
+      if(!login1){
+        login1 = res.body.message.login;
+      } else {
+        login2 = res.body.message.login;
+      }
       done();
     });
   }
@@ -144,7 +163,7 @@ function createOrganisation(done){
   function getOrganisation(done){
     chai.request(server)
       .get('/api/organisation')
-      .set('x-access-token', token)
+      .set('x-access-token', token1)
       .end(function(err, res){
         res.should.have.status(200);
         res.body.should.be.a('object');
@@ -154,11 +173,11 @@ function createOrganisation(done){
       });
     }
 
-function removeOrganisation(done){
+function removeOrganisation1(done){
   //TODO ensure removal was a success
   chai.request(server)
     .delete('/api/organisation')
-    .set('x-access-token', token)
+    .set('x-access-token', token1)
     .end(function(err, res){
       res.should.have.status(200);
       res.body.should.be.a('object');
@@ -167,8 +186,22 @@ function removeOrganisation(done){
       done();
     });
   }
+  function removeOrganisation2(done){
+    //TODO ensure removal was a success
+    chai.request(server)
+      .delete('/api/organisation')
+      .set('x-access-token', token2)
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.message.should.have.property('info');
+        res.body.message.info.should.be.a('object');
+        done();
+      });
+    }
 
 function getUser(done){
+  var token = token2 || token1;
   chai.request(server)
     .get('/api/users')
     .set('x-access-token', token)
@@ -183,7 +216,11 @@ function getUser(done){
       res.body.message.email.should.be.a('string');
       res.body.message.should.have.property('authentication');
       res.body.message.authentication.should.be.a('object');
-      uid = res.body.message._id;
+      if(!uid1){
+        uid1 = res.body.message._id;
+      } else {
+        uid2 = res.body.message._id;
+      }
       done();
     });
   }
@@ -195,7 +232,7 @@ function updateUserMetadata(done){
               "type": "metadata"};
   chai.request(server)
     .put('/api/users/' + uid)
-    .set('x-access-token', token)
+    .set('x-access-token', token1)
     .send(data)
     .end(function(err, res){
       res.should.have.status(201);
@@ -206,14 +243,14 @@ function updateUserMetadata(done){
     });
   }
 
-function updateUserVisibility(done){
+function updateUserVisibility1(done){
   var data = {"data":{
               "accessLevel": 2
             },
             "type": "visibility"};
   chai.request(server)
-  .put('/api/users/' + uid)
-  .set('x-access-token', token)
+  .put('/api/users/' + uid1)
+  .set('x-access-token', token1)
   .send(data)
   .end(function(err, res){
     res.should.have.status(201);
@@ -224,14 +261,14 @@ function updateUserVisibility(done){
   });
 }
 
-function updateUserRole(done){
+function updateUserRole1(done){
 var data = {"data":{
               "roles": ['user', 'administrator', 'service provider', 'infrastructure operator', 'system integrator', 'device owner']
             },
             "type": "roles"};
 chai.request(server)
-  .put('/api/users/' + uid)
-  .set('x-access-token', token)
+  .put('/api/users/' + uid1)
+  .set('x-access-token', token1)
   .send(data)
   .end(function(err, res){
     res.should.have.status(201);
@@ -242,7 +279,7 @@ chai.request(server)
   });
 }
 
-function createNode(done){
+function createNode1(done){
   var data = {
     "name": "testAgent",
     "type": "sharq",
@@ -250,7 +287,7 @@ function createNode(done){
   };
   chai.request(server)
     .post('/api/agents')
-    .set('x-access-token', token)
+    .set('x-access-token', token1)
     .send(data)
     .end(function(err, res){
       res.should.have.status(202);
@@ -261,10 +298,70 @@ function createNode(done){
       res.body.message.should.be.a('object');
       res.body.message.should.have.property('adid');
       res.body.message.should.have.property('id');
-      adid = res.body.message.adid;
+      adid1 = res.body.message.adid;
       done();
     });
   }
+
+  function updateUserVisibility2(done){
+    var data = {"data":{
+                "accessLevel": 2
+              },
+              "type": "visibility"};
+    chai.request(server)
+    .put('/api/users/' + uid2)
+    .set('x-access-token', token2)
+    .send(data)
+    .end(function(err, res){
+      res.should.have.status(201);
+      res.body.should.be.a('object');
+      res.body.should.have.property('success');
+      res.body.success.should.equal(true);
+      done();
+    });
+  }
+
+  function updateUserRole2(done){
+  var data = {"data":{
+                "roles": ['user', 'administrator', 'service provider', 'infrastructure operator', 'system integrator', 'device owner']
+              },
+              "type": "roles"};
+  chai.request(server)
+    .put('/api/users/' + uid2)
+    .set('x-access-token', token2)
+    .send(data)
+    .end(function(err, res){
+      res.should.have.status(201);
+      res.body.should.be.a('object');
+      res.body.should.have.property('success');
+      res.body.success.should.equal(true);
+      done();
+    });
+  }
+
+  function createNode2(done){
+    var data = {
+      "name": "testAgent2",
+      "type": "sharq",
+      "password": "password"
+    };
+    chai.request(server)
+      .post('/api/agents')
+      .set('x-access-token', token2)
+      .send(data)
+      .end(function(err, res){
+        res.should.have.status(202);
+        res.body.should.be.a('object');
+        res.body.should.have.property('error');
+        res.body.error.should.equal(false);
+        res.body.should.have.property('message');
+        res.body.message.should.be.a('object');
+        res.body.message.should.have.property('adid');
+        res.body.message.should.have.property('id');
+        adid2 = res.body.message.adid;
+        done();
+      });
+    }
 
 function createNodeNoRoles(done){
   var data = {
@@ -274,7 +371,7 @@ function createNodeNoRoles(done){
   };
   chai.request(server)
     .post('/api/agents')
-    .set('x-access-token', token)
+    .set('x-access-token', token1)
     .send(data)
     .end(function(err, res){
       res.should.have.status(403);
@@ -292,7 +389,7 @@ function createNodeNoPassword(done){
   };
   chai.request(server)
     .post('/api/agents')
-    .set('x-access-token', token)
+    .set('x-access-token', token1)
     .send(data)
     .end(function(err, res){
       res.should.have.status(400);
@@ -305,7 +402,7 @@ function createNodeNoPassword(done){
 
   function registerDevice(done){
     var data = {
-      "adid":adid,
+      "adid":adid1,
       "thingDescriptions":[
         {
         "name" : "testDev",
@@ -331,6 +428,35 @@ function createNodeNoPassword(done){
         done();
       });
     }
+
+    function registerService(done){
+      var data = {
+        "adid": adid2,
+        "thingDescriptions":[
+          {
+          "name" : "testServ",
+          "type" : "core:Service",
+          "infrastructure-id": "testServ",
+          "adapter-id": "testServ",
+          "actions" : [],
+          "properties" : [],
+          "events": []
+          }
+        ]};
+      chai.request(server)
+        .post('/commServer/items/register')
+        .send(data)
+        .end(function(err, res){
+          res.should.have.status(201);
+          res.body.should.be.a('object');
+          res.body.should.have.property('error');
+          res.body.error.should.equal(false);
+          res.body.should.have.property('message');
+          res.body.message.should.be.a('array');
+          oidSer = res.body.message[0]["nm-id"];
+          done();
+        });
+      }
 
 function registerDeviceNoAgid(done){
   var data = {
@@ -377,7 +503,24 @@ var data = {
             };
 chai.request(server)
   .put('/api/items')
-  .set('x-access-token', token)
+  .set('x-access-token', token1)
+  .send(data)
+  .end(function(err, res){
+    res.should.have.status(200);
+    res.body.should.be.a('object');
+    done();
+  });
+}
+
+function enableService(done){
+var data = {
+              "o_id": oidSer,
+              "typeOfItem": "device",
+              "status": "enabled"
+            };
+chai.request(server)
+  .put('/api/items')
+  .set('x-access-token', token2)
   .send(data)
   .end(function(err, res){
     res.should.have.status(200);
