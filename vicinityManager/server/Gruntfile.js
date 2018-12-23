e4a260604452aa2b48dee16de762c7cb17204a1a
Jorge Almela <jorgealmela@Jorges-MacBook-Pro.local>
2018-12-23 19:24:03 +0100

    Add static analizer sloc; grunt task

--- vicinityManager/server/Gruntfile.js
+++ vicinityManager/server/Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
-// Project configuration.
+// Project configuration
 	grunt.initConfig({
-
 		complexity: {
 			modules: {
 				src: ['controllers/*/*.js', 'services/*/*.js', 'services/*.js'],
@@ -33,11 +32,53 @@ module.exports = function(grunt) {
 					maintainability: 100,   // max 171 (BEST)
 					hideComplexFunctions: false,     // only display maintainability
 					broadcast: false                 // broadcast data over event-bus
+					}
+				}
+			},
+			sloc: {
+				services: {
+					// options: {
+					// 	reportType: 'json',
+					// 	reportPath: 'logs/sloc_services.json',
+					// },
+					files: {
+						'services/': ['**/**.js', '**/**.html', '*.js'],
+					}
+				},
+				controllers: {
+					// options: {
+					// 	reportType: 'json',
+					// 	reportPath: 'logs/sloc_controllers.json',
+					// },
+					files: {
+						'controllers/': ['**/**.js']
+					}
+				},
+				test: {
+					// options: {
+					// 	reportType: 'json',
+					// 	reportPath: 'logs/sloc_test.json',
+					// },
+					files: {
+						'test/': ['**/**.js'],
+					}
+				},
+				routes: {
+					// options: {
+					// 	reportType: 'json',
+					// 	reportPath: 'logs/sloc_routes.json',
+					// },
+					files: {
+						'routes/': ['**/**.js', '*.js']
+					}
 				}
 			}
-		}
 	});
-
+// Load NPM grunt modules
 	grunt.loadNpmTasks('grunt-complexity');
-	grunt.registerTask('default', [ 'complexity:modules', 'complexity:functions']);
+	grunt.loadNpmTasks('grunt-sloc');
+// Runnable tasks & groups
+	grunt.registerTask('complexityAll', [ 'complexity:modules', 'complexity:functions']);
+	grunt.registerTask('slocAll', [ 'sloc:services', 'sloc:controllers', 'sloc:test', 'sloc:routes']);
+
 };
